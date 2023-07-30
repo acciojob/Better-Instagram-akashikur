3643b50f532b6d1d7a6958e9a3e9553366809fdc
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-30 12:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-//your code here
-
+document.addEventListener("DOMContentLoaded", () => {
   const divs = document.querySelectorAll(".image");
-  let dragSrcEl = null;
+  let draggedDiv = null;
+
+  divs.forEach((div) => {
+    div.addEventListener("dragstart", handleDragStart, false);
+    div.addEventListener("dragover", handleDragOver, false);
+    div.addEventListener("drop", handleDrop, false);
+    div.addEventListener("dragend", handleDragEnd, false);
+  });
 
   function handleDragStart(e) {
-    this.style.opacity = "0.4";
-    dragSrcEl = this;
+    draggedDiv = this;
     e.dataTransfer.effectAllowed = "move";
-    e.dataTransfer.setData("text/html", this.innerHTML);
   }
 
   function handleDragOver(e) {
@@ -18,36 +22,20 @@
     return false;
   }
 
-  function handleDragEnter(e) {
-    this.classList.add("over");
-  }
-
-  function handleDragLeave(e) {
-    this.classList.remove("over");
-  }
-
   function handleDrop(e) {
     if (e.stopPropagation) {
       e.stopPropagation();
     }
-    if (dragSrcEl !== this) {
-      dragSrcEl.innerHTML = this.innerHTML;
-      this.innerHTML = e.dataTransfer.getData("text/html");
+    if (draggedDiv !== this) {
+      // Swap the background images of the two divs
+      const tempImage = draggedDiv.style.backgroundImage;
+      draggedDiv.style.backgroundImage = this.style.backgroundImage;
+      this.style.backgroundImage = tempImage;
     }
     return false;
   }
 
-  function handleDragEnd(e) {
-    this.style.opacity = "1";
-    divs.forEach((div) => div.classList.remove("over"));
+  function handleDragEnd() {
+    draggedDiv = null;
   }
-
-  divs.forEach((div) => {
-    div.addEventListener("dragstart", handleDragStart, false);
-    div.addEventListener("dragenter", handleDragEnter, false);
-    div.addEventListener("dragover", handleDragOver, false);
-    div.addEventListener("dragleave", handleDragLeave, false);
-    div.addEventListener("drop", handleDrop, false);
-    div.addEventListener("dragend", handleDragEnd, false);
-  });
-
+});
